9f331f20c5b791fc7489f7a9b231fe5ab96e9c03
Venkat Sai Jarugula <jarugulavenkat7@gmail.com>
2026-04-03 22:11:35 -0700

    scroll fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
     const handleScroll = () => {
       setScrolled(window.scrollY > 80);
     };
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll, { passive: true });
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
